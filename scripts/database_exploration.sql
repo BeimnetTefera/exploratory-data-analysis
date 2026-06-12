a102b1b3aea01925ed107e1de50441d88f65b95b
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-12 16:24:42 +0300

    Update database_exploration.sql

--- scripts/database_exploration.sql
+++ scripts/database_exploration.sql
@@ -1,1 +1,17 @@
+-- Explore All Objects in the Database
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES;
 
+-- Explore All Column in the Database
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_products';
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'fact_sales'
