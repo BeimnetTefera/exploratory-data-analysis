004124d5e0bd2601aa6d8264a0d2f42b322e19fc
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-13 11:15:23 +0300

    Update date_exploration.sql

--- scripts/date_exploration.sql
+++ scripts/date_exploration.sql
@@ -1,1 +1,18 @@
+----------------------------------------------------
+-------------- DATE EXPLORATION  -------------------
+----------------------------------------------------
 
+-- FIND THE DATE OF FIRST ORDER AND LAST ORDER
+SELECT 
+	MIN(order_date) AS FirsOrder,
+	MAX(order_date) AS LastOrder,
+	DATEDIFF(YEAR, MIN(order_date), MAX(order_date) ) AS YearlyOrderSpan
+FROM [dbo].[fact_sales]
+
+-- FIND THE YOUNGEST AND OLDEST CUSTOMER
+SELECT 
+	MIN(birthdate) AS OldestBirthdate,
+	DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS OldestAge,
+	MAX(birthdate) AS YoungestBirthdate,
+	DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS YoungestAge
+FROM[dbo].[dim_customers]
