b9fc5fddaf564acf1e14c770352af71b90df12d1
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-13 10:59:50 +0300

    Add dimension exploration queries for customers and products
    Added SQL queries to explore the dimensions of customer and product tables, including distinct values for various attributes.

--- scripts/dimension_exploration.sql
+++ scripts/dimension_exploration.sql
@@ -1,1 +1,49 @@
+----------------------------------------------------
+------------ DIMENCTION EXPLORATION  ---------------
+----------------------------------------------------
+-- HELPS US TO UNDERSTAND THE GRANULARITY OF EACH COLLUMN
 
+	--- EXPLORE CUSTOMER TABLE ---
+SELECT *
+FROM [dbo].[dim_customers]
+
+-- EXPLORE ALL COUNTRIES THE CUSTOMERS COME FROM
+SELECT 
+	DISTINCT country
+FROM [dbo].[dim_customers]
+
+-- EXPLORE THE GENDER OF CUSTOMERS
+SELECT 
+	DISTINCT gender
+FROM [dbo].[dim_customers]
+
+
+	--- EXPLORE PRODUCT TABLE ---
+SELECT *
+FROM [dbo].[dim_products]
+
+-- EXPLORE ALL PRODUCT CATEGORY
+SELECT 
+	DISTINCT category
+FROM [dbo].[dim_products]
+
+-- EXPLORE ALL PRODUCT SUBCATEGORY
+SELECT 
+	DISTINCT subcategory
+FROM [dbo].[dim_products]
+
+-- EXPLORE ALL PRODUCT LINE
+SELECT 
+	DISTINCT product_line
+FROM [dbo].[dim_products]
+
+-- EXPLORE ALL PRODUCT MAINTENANCE
+SELECT
+	DISTINCT maintenance
+FROM [dbo].[dim_products]
+
+-- SEE THE BIG PICTURE OF PRODUCT TABLE 
+SELECT 
+	DISTINCT category, subcategory, product_name
+FROM [dbo].[dim_products]
+ORDER BY category, subcategory, product_name
