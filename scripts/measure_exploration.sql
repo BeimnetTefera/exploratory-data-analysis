1ac4681f2b79d173e9a575e46adb395b2488e625
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-13 12:09:18 +0300

    Add SQL queries for sales and customer metrics
    Added SQL queries to calculate total sales, items sold, average selling price, total orders, total customers, total products, and customers who placed an order.

--- scripts/measure_exploration.sql
+++ scripts/measure_exploration.sql
@@ -1,1 +1,34 @@
+-- FIND THE TOTAL SALES
+SELECT 
+	FORMAT(SUM(sales_amount), 'c') AS TotalSales
+FROM [dbo].[fact_sales]
 
+-- TOTAL NUMBER OF ITEMS ARE SOLD
+SELECT 
+	SUM(quantity) AS TotalItemsSold
+FROM [dbo].[fact_sales]
+
+-- AVERAGE SELLING PRICE
+SELECT 
+	AVG(price) AS AverageSellingPrice
+FROM [dbo].[fact_sales]
+
+-- TOTAL NUMBER OF ORDERS
+SELECT 
+	COUNT(order_number) AS TotalOrders
+FROM [dbo].[fact_sales]
+
+-- TOTAL NUMBER OF CUSTOMERS
+SELECT 
+	COUNT(DISTINCT customer_key) AS TotalCustomers
+FROM [dbo].[dim_customers]
+
+-- TOTAL NUMBER OF PRODUCTS
+SELECT 
+	COUNT(product_key) AS TotalProducts
+FROM [dbo].[dim_products]
+
+-- TOTAL NUMBER OF CUSTOMERS WHO HAVE PLACED AN ORDER 
+SELECT 
+	COUNT(DISTINCT customer_key) TotalCustomerPlacedOrder
+FROM [dbo].[fact_sales]
