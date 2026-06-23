35d5b0a4dc87718fdb5217208c287cd62f0476d8
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-23 14:06:57 +0300

    Update 10_part_to_whole_analysis.sql

--- scripts/10_part_to_whole_analysis.sql
+++ scripts/10_part_to_whole_analysis.sql
@@ -1,1 +1,39 @@
+-- Which Category contribute the most to overall sales
+WITH CategorySales AS (
+    SELECT 
+        prd.category,
+        SUM(sales_amount) AS SalesByProducts
+    FROM [dbo].[fact_sales] AS sls
+    LEFT JOIN [dbo].[dim_products] AS prd 
+        ON sls.product_key = prd.product_key
+    GROUP BY prd.category
+)
+SELECT 
+    category,
+    ROUND(
+        (
+            CAST(SalesByProducts AS FLOAT) 
+            / SUM (SalesByProducts) OVER ()
+        ) * 100
+        , 2) AS SalesPercentage
+FROM CategorySales
+ORDER BY SalesPercentage DESC
 
+-- Another Way 
+-- BRUTEFORCE APPROACH
+WITH ProductSales AS (
+    SELECT DISTINCT
+        prd.category,
+        SUM(sls.sales_amount) OVER (PARTITION BY prd.category) AS SalesByProduct,
+        SUM(sls.sales_amount) OVER () AS TotalSales
+    FROM [dbo].[fact_sales] AS sls
+    LEFT JOIN [dbo].[dim_products] AS prd 
+        ON sls.product_key = prd.product_key
+)
+SELECT 
+    category,
+    ROUND (
+    (CAST(SalesByProduct AS float) / TotalSales) * 100 , 2 )
+    AS SalesPercentage
+FROM ProductSales
+ORDER BY SalesPercentage DESC
